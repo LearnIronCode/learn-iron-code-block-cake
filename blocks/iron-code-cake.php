40819f2bda6b767554a6a44b290fd14022353a8a
Sal Ferrarello <sal@ironcodestudio.com>
2018-05-11 15:35:30 -0400

    Initial commit
    Result of running two WP CLI commands.
    
    wp scaffold plugin learn-iron-code-block-cake --skip-tests --plugin_name="Learn Iron Code Block Cake" --plugin_author="Sal Ferrarello" --plugin_author_uri="https://salferrarello.com/" --plugin_description="Gutenberg Block Example" --plugin_uri="https://salferrarello.com/wordpress-custom-editor-block/"
    
    wp scaffold block iron-code-cake --title="Iron Code Cake" --plugin=learn-iron-code-block-cake

---
+++ blocks/iron-code-cake.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Functions to register client-side assets (scripts and stylesheets) for the
+ * Gutenberg block.
+ *
+ * @package learn-iron-code-block-cake
+ */
+
+/**
+ * Registers all block assets so that they can be enqueued through Gutenberg in
+ * the corresponding context.
+ *
+ * @see https://wordpress.org/gutenberg/handbook/blocks/writing-your-first-block-type/#enqueuing-block-scripts
+ */
+function iron_code_cake_block_init() {
+	$dir = dirname( __FILE__ );
+
+	$index_js = 'iron-code-cake/index.js';
+	wp_register_script(
+		'iron-code-cake-block-editor',
+		plugins_url( $index_js, __FILE__ ),
+		array(
+			'wp-blocks',
+			'wp-i18n',
+			'wp-element',
+		),
+		filemtime( "$dir/$index_js" )
+	);
+
+	$editor_css = 'iron-code-cake/editor.css';
+	wp_register_style(
+		'iron-code-cake-block-editor',
+		plugins_url( $editor_css, __FILE__ ),
+		array(
+			'wp-blocks',
+		),
+		filemtime( "$dir/$editor_css" )
+	);
+
+	$style_css = 'iron-code-cake/style.css';
+	wp_register_style(
+		'iron-code-cake-block',
+		plugins_url( $style_css, __FILE__ ),
+		array(
+			'wp-blocks',
+		),
+		filemtime( "$dir/$style_css" )
+	);
+
+	register_block_type( 'learn-iron-code-block-cake/iron-code-cake', array(
+		'editor_script' => 'iron-code-cake-block-editor',
+		'editor_style'  => 'iron-code-cake-block-editor',
+		'style'         => 'iron-code-cake-block',
+	) );
+}
+add_action( 'init', 'iron_code_cake_block_init' );
---
+++ learn-iron-code-block-cake.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Learn Iron Code Block Cake
+ * Plugin URI:      https://salferrarello.com/wordpress-custom-editor-block/
+ * Description:     Gutenberg Block Example
+ * Author:          Sal Ferrarello
+ * Author URI:      https://salferrarello.com/
+ * Text Domain:     learn-iron-code-block-cake
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Learn_Iron_Code_Block_Cake
+ */
+
+// Your code starts here.
